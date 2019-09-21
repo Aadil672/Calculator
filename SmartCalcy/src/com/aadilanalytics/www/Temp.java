a3486eb5791f7b16e0390c45955d9b413ae6a8d4
anujpraj <anuj.prajapati@yantriks.com>
2019-09-22 00:01:42 +0530

    Gaad Marao AADIL

--- SmartCalcy/src/com/aadilanalytics/www/Temp.java
+++ SmartCalcy/src/com/aadilanalytics/www/Temp.java
@@ -3,5 +3,6 @@ package com.aadilanalytics.www;
 public class Temp {
     public static void main(String args[]) {
         System.out.println("HeyAadil");
+        System.out.println("Gaad Marao Aadil");
     }
 }
