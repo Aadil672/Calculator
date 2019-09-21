6395e479e28c6b80a6306e1aa7d83c37c4624852
anujpraj <anuj.prajapati@yantriks.com>
2019-09-22 00:19:26 +0530

    Phir Gaad Marao Aadil

--- SmartCalcy/src/com/aadilanalytics/www/Temp.java
+++ SmartCalcy/src/com/aadilanalytics/www/Temp.java
@@ -4,5 +4,6 @@ public class Temp {
     public static void main(String args[]) {
         System.out.println("HeyAadil");
         System.out.println("Gaad Marao Aadil");
+        System.out.println("Gaad Marao Aadil Phir se");
     }
 }
